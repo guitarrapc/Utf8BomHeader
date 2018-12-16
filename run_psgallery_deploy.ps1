5ff69dc1d18c0939cf87c4c72679c91572d30571
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2018-12-17 02:53:46 +0900

    fix(ci) set trigger branch

--- run_psgallery_deploy.ps1
+++ run_psgallery_deploy.ps1
@@ -1,27 +1,21 @@
 #!/usr/bin/env pwsh
 [OutputType([void])]
 param (
-    [string]$Version,
-    [string]$NuGetApiKey
+    [string]$NuGetApiKey,
+    [string]$BuildBranch
 )
-$branch = "master"
-if ($env:APPVEYOR_REPO_BRANCH -notmatch $branch) {
+
+# validation
+if ($env:APPVEYOR_REPO_BRANCH -notmatch $BuildBranch) {
     Write-Host "`"Appveyor`" deployment has been skipped as environment variable has not matched (`"$env:APPVEYOR_REPO_BRANCH`" is `"$branch`", should be `"$branch`""
     exit
 }
-
 if ([string]::IsNullOrWhiteSpace($env:APPVEYOR_REPO_TAG)) {
-    Write-Host "`"Appveyor`" deployment has been skipped as environment variable has not matched (`"$env:APPVEYOR_REPO_TAG`" is blank."
+    Write-Host "`"Appveyor`" deployment has been skipped as environment variable has not matched (`"$env:APPVEYOR_REPO_TAG`" is blank)"
     exit
 }
-
-if ([string]::IsNullOrWhiteSpace($Version)) {
-    Write-Warning "Version not specified, please specify semantic version."
-    return;
-}
-
 if ([string]::IsNullOrWhiteSpace($NuGetApiKey)) {
-    Write-Warning "NuGetApiKey not specified, please specify NuGetApiKey."
+    Write-Warning "`"Appveyor`" deployment has been skipped as `"NuGetApiKey`" is not specified."
     return;
 }
 
@@ -31,10 +25,11 @@ Write-Host 'Running AppVeyor deploy script' -ForegroundColor Green
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $moduleName = Split-Path -Path $here -Leaf
 $modulePath = Join-Path "$here" "publish/Utf8BomHeader"
+$version = $env:APPVEYOR_REPO_TAG_NAME
 
 # Update module manifest # 
 Write-Host 'Creating new module manifest'
-. ./run_build.ps1 -Version $Version
+. ./run_build.ps1 -Version $version
 
 # Publish to PS Gallery # 
 Write-Host "Adding $modulePath to 'psmodulepath' PATH variable"
